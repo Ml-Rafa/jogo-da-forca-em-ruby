ecc571dbdaa3ed22db83b94875383092a4e4e3e5
Ml-Rafa <rafael.aoliveira@mercadolivre.com>
2023-01-07 12:15:30 -0300

    update code

--- forca.rb
+++ forca.rb
@@ -1,82 +1,43 @@
+def escolha_uma_letra
+  p "De seu primeiro pitaco"
+  chute =  gets.strip.downcase
 
-  def boas_bindas
-    p "Bem vindo ao jogo da forca"
-    p "Qual é seu nome ?"
-    nome = gets.strip
-    p "\n\n\n\n\n\n\n"
-    p "começaremos um jogo para voce #{nome}"
-    nome
-  end
+  palavra_secreta = "programador"
+  chutes = []
+  erros = 0
+  acertos = 0
 
-  def escolhendo_palavras
-    p "escolhendo uma palavra"
-    palavra_secreta = "Programado"
-    p "Palavra secreta com #{palavra_secreta.size} letras "
-    palavra_secreta
-  end
-
-  def nao_quer_jogar?
-    p "Quer jogar novamente ? (S/N)"
-    jogar_novamente = gets.strip
-    # nao_quero_jogar = jogar_novamente.upcase('N') sera que posso fazer assim ?
-    nao_quero_jogar = jogar_novamente.upcase == 'N'
-
-  end
-
-  def dar_chutes(chutes)
-    p "Bora jogar ? digite uma letra ou palavra ..."
-    p "voce chutou #{chutes}"
-    chute = gets.strip
-    chute
-  end
-
-  def joga(nome)
-    palavra_secreta = escolhendo_palavras
-
-    erros = 0
-    chutes = []
-    pontos = 0
-
-    while erros < 5
-      chute  =  dar_chutes chutes
+  while erros < 5
+    if palavra_secreta.include?(chute)
+      p "Voce acertou uma letra"
       chutes << chute
-      
-      chutou_uma_letra =  chute.size == 1
-      if chutou_uma_letra
-        letraprocurada = chute[0]
-        total_encontrada = palavra_secreta.count letraprocurada
-        if total_encontrada == 0
-          p "Letra nao encontrada"
-          erros += 1
-        else
-          p "Essa letra tem"
-        end
-      else
-
-        acertou = chute == palavra_secreta
-        if acertou
-          p "Parabens voce acertou a palavra secreta"
-          pontos += 100
-          break
-        else
-          p "voce errou a palavra secreta"
-          pontos -= 30
-          erros += 1
-        end
+      acertos += 1
+      if chute == palavra_secreta
+        p "Voce acertou a palavra secreta "
       end
-      
-    end
-    
-    p "voce acertou #{pontos} e errou #{erros} e voce chutou #{chutes}"
-  end
-
-  nome = boas_bindas
-  loop do
-    joga nome
-    if nao_quer_jogar?
-      p "obrigado por jogar"
       break
+      p chutes
+      chute = gets.strip
+    else
+      p "voce errou uma letra"
+      chutes << chute
+      erros += 1
+      p chutes
+      chute = gets.strip
     end
   end
-  
-  
+end
+
+def saldação
+  p 'Vamos jogar ? digite (S/N)'
+  resposta =  gets.strip
+  if resposta.upcase == 'N'
+    p "Obrigado até mais"
+  else
+    p "Que legal, vamos jogar"
+    escolha_uma_letra
+  end
+end
+saldação
+
+
